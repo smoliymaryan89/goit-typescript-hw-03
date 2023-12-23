b1b78ae5d780a2705c5e7d44545eac46f6c4409c
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-12-23 17:37:23 +0200

    Update 1.ts

--- src/1.ts
+++ src/1.ts
@@ -5,15 +5,17 @@
 */
 
 class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
+  // public name: string;
+  // public age: number;
+  // public grade: string;
 
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
+  // constructor(name: string, age: number, grade: string) {
+  //   this.name = name;
+  //   this.age = age;
+  //   this.grade = grade;
+  // }
+
+  constructor(public name: string, public age: number, public grade: string) {}
 }
 
 export {};
