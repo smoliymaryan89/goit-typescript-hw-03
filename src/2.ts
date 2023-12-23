c21c6824851eb2637c4feb208eb0225b7fe0b5c8
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-12-23 17:37:29 +0200

    Update 2.ts

--- src/2.ts
+++ src/2.ts
@@ -17,8 +17,8 @@
 class Employee {
   // Заповніть модифікатори доступу
   name: string;
-  department: string;
-  salary: number;
+  private department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -33,7 +33,13 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
 }
 
+const manager = new Manager("Maryan", "IT", 5000);
+console.log(manager.getEmployeeDetails());
 
 export {};
